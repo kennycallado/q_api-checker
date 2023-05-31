ef268664e2d1c6b2cb88ae5bcdde9b56f5787feb
kennycallado <kennycallado@hotmail.com>
2023-05-31 12:52:34 +0200

    VERSION

--- src/routing.ts
+++ src/routing.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express'
 
 // import { getProject, getProjectCron, getProjectPush, getProjects, postProjectCron, postProjectPush } from './modules/project/controller'
-import { postProjectCron, postProjectPush } from './modules/project/controller'
+import { /*postProjectCron,*/ postProjectPush } from './modules/project/controller'
 
 
 const router: Router = Router()
@@ -12,7 +12,7 @@ const router: Router = Router()
 // router.get('/project/:id/cron', getProjectCron)
 // router.get('/project/:id/push', getProjectPush)
 
-router.post('/project/:id/cron', postProjectCron)
+// router.post('/project/:id/cron', postProjectCron)
 router.post('/project/:id/push', postProjectPush)
 
 export default router
