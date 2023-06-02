d7ef981079d9d191dc04099a021ec89702ab42f8
kennycallado <kennycallado@hotmail.com>
2023-06-02 13:02:40 +0200

    UPDATE Actualiza controlador push y manejador para cron

--- src/modules/project/controller.ts
+++ src/modules/project/controller.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from 'express'
 
-import { getPushProjectScript } from '../../services/scripts'
+import { getCronProjectScript, getPushProjectScript } from '../../services/scripts'
 
 import { User } from '../../providers/models'
-import { PostPaper } from '../../providers/types'
+import { PubPaperPush } from '../../providers/types'
 
 // export async function getProjects(_req: Request, res: Response) {
 //   res.json({ message: 'index' })
@@ -24,24 +24,71 @@ import { PostPaper } from '../../providers/types'
 //   res.json({ message: `show push project, ${id}!` })
 // }
 
-export async function postProjectCron(req: Request, res: Response) {
-  const id: string = req.params.id
-  res.json({ message: `Hello, ${id}!` })
-}
-
-export async function postProjectPush(req: Request, res: Response) {
+export async function postProjectCron(req: Request, res: Response): Promise<Response> {
   /* Validation */
-  if (isNaN(parseInt(req.params.id))) {
+  if (isNaN(parseInt(req.params.project_id))) {
     return res.status(400).json({ error: 'Bad request' })
   }
-  const project_id: number = parseInt(req.params.id)
+  const project_id: number = parseInt(req.params.project_id)
+  const cron_name: string = req.params.cron_name
+
+  // Validate array
+  if (!Array.isArray(req.body)) {
+    return res.status(400).json({ error: 'Bad request' })
+  }
+
+  // Validate array items
+  for (let i = 0; i < req.body.length; i++) {
+    if (
+      req.body[i].id           === undefined ||
+      req.body[i].user_id      === undefined ||
+      req.body[i].user_record  === undefined ||
+      req.body[i].project_id   === undefined ||
+      req.body[i].resource_id  === undefined ||
+      req.body[i].completed    === undefined ||
+      req.body[i].answers      === undefined
+    ) {
+      return res.status(400).json({ error: 'Bad request' })
+    }
+  }
+
+  let projectScript = getCronProjectScript(project_id, cron_name)
+  if (!projectScript) {
+    return res.status(404).json({ error: `Cron script -${cron_name}- for project ${project_id} not found` })
+  }
+  /* fin Validation */
+
+  let papers: any = []
+  for (let i = 0; i < req.body.length; i++) {
+    let user: User = Function(
+      await import('../../providers/models').then((m) => m.User.toString())
+        + `const user = new User(${JSON.stringify(req.body[i])});`
+        + `function main() {`
+        + projectScript
+        + `};`
+        + `main(); return user;`
+
+    )()
+    let paper = userToPaperPush(user)
+    papers.push(Object.assign(paper , { actions: user.actions }))
+  }
+
+  /* Post */
+  return res.json(papers)
+}
+
+export async function postProjectPush(req: Request, res: Response): Promise<Response> {
+  /* Validation */
+  if (isNaN(parseInt(req.params.project_id))) {
+    return res.status(400).json({ error: 'Bad request' })
+  }
+  const project_id: number = parseInt(req.params.project_id)
 
   if (
     req.body.id           === undefined ||
     req.body.user_id      === undefined ||
     req.body.user_record  === undefined ||
     req.body.project_id   === undefined ||
-    // req.body.project_keys === undefined ||
     req.body.resource_id  === undefined ||
     req.body.completed    === undefined ||
     req.body.answers      === undefined
@@ -57,24 +104,26 @@ export async function postProjectPush(req: Request, res: Response) {
 
   let user: User = Function(
     await import('../../providers/models').then((m) => m.User.toString())
-    + `const user = new User(${JSON.stringify(req.body)});`
-    + projectScript
-    + `return main();`
-  )();
+      + `const user = new User(${JSON.stringify(req.body)});`
+      + `function main() {`
+      + projectScript
+      + `};`
+      + `main(); return user;`
+
+  )()
 
   /* Post */
-  let paper = userToPostPaper(user)
+  let paper = userToPaperPush(user)
 
   return res.json(Object.assign(paper, { actions: user.actions }))
 }
 
-function userToPostPaper(user: User): PostPaper {
+function userToPaperPush(user: User): PubPaperPush {
   return {
     id: user.paper.id,
     user_id: user.paper.user_id,
     user_record: user.record,
     project_id: user.paper.project_id,
-    // project_keys: user.paper.project_keys,
     resource_id: user.paper.resource_id,
     completed: user.paper.completed,
     answers: user.answers,
