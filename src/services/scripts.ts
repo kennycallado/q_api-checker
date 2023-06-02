532266becd5a7d1d8a4721d0415c7d92a87ce781
kennycallado <kennycallado@hotmail.com>
2023-06-02 12:57:25 +0200

    ADD Agrega función para obtener cron scripts

--- src/services/scripts.ts
+++ src/services/scripts.ts
@@ -1,29 +1,22 @@
 import fs from 'fs'
 import path from 'path'
 
-function getCronProjectScript(project_id: string) {
-    return fs.readFileSync(path.join(process.cwd(), 'scripts', project_id, 'cron.js'), 'utf8')
+function getCronProjectScript(project_id: number, cron_name: string) {
+  try {
+    return fs.readFileSync(path.join(process.cwd(), 'scripts', project_id.toString(), 'cron', `${cron_name}.js`), 'utf8')
+  } catch (err) {
+    console.error(err.message)
+    return null
+  }
 }
 
 function getPushProjectScript(project_id: number) {
   try {
     return fs.readFileSync(path.join(process.cwd(), 'scripts', project_id.toString(), 'push.js'), 'utf8')
   } catch (err) {
-    console.error(err)
+    console.error(err.message)
     return null
   }
 }
-
-// function getPushSystemScript() {
-//   let ext = process.env.NODE_ENV === 'development' ? 'ts' : 'js'
-//   let scriptPath = path.join(__dirname, '../providers', 'scripts', `push.${ext}`)
-
-//   try {
-//     return fs.readFileSync(scriptPath, 'utf8')
-//   } catch (err) {
-//     console.error(err)
-//     return null
-//   }
-// }
 
 export { getCronProjectScript , getPushProjectScript }
