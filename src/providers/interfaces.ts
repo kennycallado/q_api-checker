dc6eb1cf9c829ac67e9989898809c67a38856d52
kennycallado <kennycallado@hotmail.com>
2023-06-02 13:01:27 +0200

    UPDATE Actualiza User e interface nuevo método send_message

--- src/providers/interfaces.ts
+++ src/providers/interfaces.ts
@@ -1,6 +1,7 @@
 import { User } from './types';
 
 export interface IUser extends User {
+  send_message(message_id: number): void;
   add_resource(...resource_ids: number[]): void;
   resource_complete(...resource_ids: number[]): void;
 }
--- src/providers/models.ts
+++ src/providers/models.ts
@@ -1,35 +1,35 @@
-import { Action, Answer, Paper, PostPaper} from "./types";
-import { IUser } from "./interfaces";
+import { Action, PubNewAnswer, Paper, PubPaperPush} from "./types"
+import { IUser } from "./interfaces"
 
-// Clase más por legibilidad que por funcionalidad
-// para therapist es más fácil entender que se hacer
-// con un User que con un PostPaper
 export class User implements IUser {
-  readonly id;
-  readonly record;
-  readonly answers: Answer[];
-  readonly actions: Action[];
-  readonly paper: Paper;
-  // project_keys; // needed ??
+  readonly id
+  readonly record
+  readonly answers: PubNewAnswer[]
+  readonly actions: Action[]
+  readonly paper: Paper
 
-  constructor(paper: PostPaper) {
-    // this.project_keys = paper.project_keys;
-    this.id = paper.user_id;
-    this.record = paper.user_record;
-    this.answers = paper.answers;
-    this.actions = [];
+  constructor(paper: PubPaperPush) {
+    this.id = paper.user_id
+    this.record = paper.user_record
+    this.answers = paper.answers
+    this.actions = []
     this.paper = {
       id: paper.id,
       user_id: paper.user_id,
       project_id: paper.project_id,
       resource_id: paper.resource_id,
       completed: paper.completed,
-    };
+    }
+  }
+
+  send_message(message_id: number) {
+    this.actions.push({ action: "send_message", params: [message_id] })
+    return
   }
 
   add_resource(...resource_ids: number[]) {
     if (typeof resource_ids[0] !== "number") {
-      resource_ids = resource_ids[0];
+      resource_ids = resource_ids[0]
     }
 
     this.actions.push({ action: "add_resource", params: resource_ids })
@@ -38,13 +38,11 @@ export class User implements IUser {
 
   resource_complete(...resource_ids: number[]) {
     if (resource_ids.length === 0 || resource_ids.find((id) => id === this.paper.resource_id)) {
-      this.paper.completed = true;
+      this.paper.completed = true
       return
     }
 
     this.actions.push({ action: "resource_complete", params: resource_ids })
     return
   }
-
-  // get actions() { return this.actions; }
 }
