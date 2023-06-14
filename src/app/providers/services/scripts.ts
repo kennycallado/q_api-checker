4f3c54e9ced15edab8066e4263b1c080d7046a74
kennycallado <kennycallado@hotmail.com>
2023-06-14 10:59:04 +0200

    FIX ref to SCRIPTS/

--- src/app/providers/services/scripts.ts
+++ src/app/providers/services/scripts.ts
@@ -3,7 +3,7 @@ import path from 'path'
 
 function getCronProjectScript(project_id: number, cron_name: string) {
   try {
-    return fs.readFileSync(path.join(process.cwd(), 'scripts', project_id.toString(), 'cron', `${cron_name}.js`), 'utf8')
+    return fs.readFileSync(path.join(process.cwd(), 'SCRIPTS', project_id.toString(), 'cron', `${cron_name}.js`), 'utf8')
   } catch (err) {
     console.error(err.message)
     return null
@@ -12,7 +12,7 @@ function getCronProjectScript(project_id: number, cron_name: string) {
 
 function getPushProjectScript(project_id: number) {
   try {
-    return fs.readFileSync(path.join(process.cwd(), 'scripts', project_id.toString(), 'push.js'), 'utf8')
+    return fs.readFileSync(path.join(process.cwd(), 'SCRIPTS', project_id.toString(), 'push.js'), 'utf8')
   } catch (err) {
     console.error(err.message)
     return null
